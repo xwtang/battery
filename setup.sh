1cf5d6196383c8f50452819a87f497869d072c5e
Xiaowen Tang <xtang@icloud.com>
2022-11-13 16:24:38 +0800

    modify install procedure

--- setup.sh
+++ setup.sh
@@ -35,16 +35,16 @@ sudo mv $smcfolder/smc-command/smc $binfolder
 sudo chmod u+x $binfolder/smc
 
 # Write battery function as executable
-bateryfolder="$tempfolder/battery"
+batteryfolder="$HOME/battery"
 echo "[ 5/9 ] Cloning battery repository"
-git clone --depth 1 https://github.com/xtang/battery.git $bateryfolder &> /dev/null
+# git clone --depth 1 https://github.com/xtang/battery.git $batteryfolder &> /dev/null
 echo "[ 6/9 ] Writing script to $binfolder/battery for user $calling_user"
-sudo cp $bateryfolder/battery.sh $binfolder/battery
+sudo cp $batteryfolder/battery.sh $binfolder/battery
 sudo chown $calling_user $binfolder/battery
 sudo chmod 755 $binfolder/battery
 sudo chmod u+x $binfolder/battery
 
-sudo bash $bateryfolder/battery.sh visudo
+sudo bash $batteryfolder/battery.sh visudo
 echo "[ 7/9 ] Set up visudo declarations"
 
 # Remove tempfiles
